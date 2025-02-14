5ebdc7af002d35f406a00d256fe136a4965ee689
Dagim <dagimgetaW27@gmail.com>
2025-02-14 22:44:43 +0300

    add whatwedo page components

---
+++ src/Components/WhatWeDo/Aim.jsx
@@ -0,0 +1,60 @@
+import { FaUsers, FaUserGraduate } from "react-icons/fa";
+
+export default function Aim() {
+  return (
+    <div className="py-12 px-6 md:px-12 lg:px-20 xl:px-36 bg-gray-100">
+      <div className="flex flex-col md:flex-row gap-10 items-stretch pb-15">
+        {/* Quality Improvement Programs */}
+        <div className="bg-white shadow-lg rounded-2xl p-6 flex-1 flex flex-col">
+          <FaUsers className="m-auto text-5xl md:text-6xl text-gray-700" />
+          <h2 className="text-2xl font-semibold mt-4 font-text text-center">
+            Quality Improvement Programs
+          </h2>
+          <p className="mt-4 font-text text-md md:text-lg leading-8 md:leading-9 flex-grow">
+            HELP Ethiopia demonstrates a commitment to continuous improvement by
+            implementing three impactful programs focused on streamlining
+            healthcare processes, enriching data-driven decision-making, and
+            optimizing patient experience.
+          </p>
+          <ul className="text-md md:text-lg leading-8 md:leading-9 list-disc list-inside font-text pl-6 pt-4">
+            <li className="pb-2 leading-7 md:leading-9">
+              Operating Room Efficiency Improvement
+            </li>
+            <li className="pb-2 leading-7 md:leading-9">
+              Improving Hospital Transfers
+            </li>
+            <li className="pb-2 leading-7 md:leading-9">
+              Data Registry Enhancement
+            </li>
+          </ul>
+        </div>
+
+        {/* Education and Leadership Programs */}
+        <div className="bg-white shadow-lg rounded-2xl p-6 flex-1 flex flex-col">
+          <FaUserGraduate className="m-auto text-5xl md:text-6xl text-gray-700" />
+          <h2 className="text-2xl font-semibold mt-4 font-text text-center">
+            Education and Leadership Programs
+          </h2>
+          <p className="mt-4 font-text text-md md:text-lg leading-8 md:leading-9 flex-grow">
+            HELP Ethiopia recognizes that building a strong healthcare system
+            requires not just immediate interventions, but also investment in
+            the future.
+          </p>
+          <p className="mt-2 font-text text-base md:text-lg leading-7 md:leading-9">
+            Our Education and Leadership program focuses on empowering the next
+            generation of Ethiopians to become effective leaders and agents of
+            positive change within the healthcare sector.
+          </p>
+          <ul className="text-base md:text-lg list-disc list-inside font-text pl-6 pt-4">
+            <li className="pb-2 leading-7 md:leading-9">
+              Research Mentorship Program
+            </li>
+            <li className="pb-2 leading-7 md:leading-9">
+              Youth Capacity Building for Leadership
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/WhatWeDo/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="pt-80 pb-100 text-center bg-[#e9e9e9]">
+      <h2 className="pb-4 text-2xl font-title">A few words about</h2>
+      <p className="pb-12 text-9xl font-text font-semibold text-[#F1C40F]">
+        What We Do
+      </p>
+    </div>
+  );
+}
