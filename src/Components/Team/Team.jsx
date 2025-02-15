5d24b388a4acbac06a86ce812627ecfd6bfb97e2
Dagim <dagimgetaW27@gmail.com>
2025-02-15 11:19:38 +0300

    remove Link tag, add lazy loading and update alt msg

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,10 +1,9 @@
 import { FaFacebookF, FaLinkedinIn, FaGoogle } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import teamData from "./People";
 
 export default function Team() {
   return (
-    <div className="pt-40 pb-5 pl-10 pr-5 md:px-12 lg:px-20 xl:px-35">
+    <div className="pt-40 pb-15 pl-10 pr-5 md:px-12 lg:px-20 xl:px-35">
       <h2 className="text-3xl md:text-4x xl:text-5xl 2xl:text-6xl font-bold text-center text-gray-800">
         Meet Our Team!
       </h2>
@@ -13,7 +12,6 @@ export default function Team() {
         backgrounds, sharing a common goal. We are passionate and dedicated to
         making a positive impact in our community.
       </p>
-
       <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 pb-20">
         {teamData.map((team) => (
           <div
@@ -22,27 +20,18 @@ export default function Team() {
           >
             <img
               src={team.image}
-              alt={team.name}
+              alt={`Help Ethiopia ${team.name} image`}
+              loading="lazy"
               className="w-40 h-40 mx-auto rounded-full object-cover border-4 border-[#1E3A8A]"
             />
             <h3 className="mt-4 text-2xl font-text font-semibold text-gray-800">
               {team.name}
             </h3>
             <p className="text-gray-600 text-sm font-text">{team.position}</p>
-            {/* <p className="text-gray-500 text-xs mt-2 font-text">
-              {team.Biography}
-            </p> */}
-
             <div className="flex justify-center gap-4 mt-4 text-[#1E3A8A] text-xl">
-              <Link to={team.facebook} className="hover:text-[#F1C40F]">
-                <FaFacebookF />
-              </Link>
-              <Link to={team.linkedin} className="hover:text-[#F1C40F]">
-                <FaLinkedinIn />
-              </Link>
-              <Link to={team.gmail} className="hover:text-[#F1C40F]">
-                <FaGoogle />
-              </Link>
+              <FaFacebookF className="hover:text-[#F1C40F] cursor-pointer" />
+              <FaLinkedinIn className="hover:text-[#F1C40F] cursor-pointer" />
+              <FaGoogle className="hover:text-[#F1C40F] cursor-pointer" />
             </div>
           </div>
         ))}
