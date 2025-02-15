bb53f54808d45a469b2f6fffd1fd3c40fb796f28
Dagim <dagimgetaW27@gmail.com>
2025-02-15 11:30:27 +0300

    add lazy loading

--- src/Components/Donate/Home.jsx
+++ src/Components/Donate/Home.jsx
@@ -4,23 +4,18 @@ import abyssinia from "../../assets/abyssinia.png";
 export default function Home() {
   return (
     <div className="pt-40 pb-5 px-10 md:px-12 lg:px-20 xl:px-35 bg-gray-100">
-      {/* Donation Header */}
       <h2 className="text-3xl md:text-4x xl:text-5xl 2xl:text-6xl font-text font-semibold text-gray-800 text-center">
         Make a Donation
       </h2>
-
-      {/* Image and Description Section */}
       <div className="flex flex-col lg:flex-row gap-8 pt-12">
-        {/* Image */}
         <div className="flex justify-center lg:w-1/2">
           <img
             src={member}
-            alt="Team member of Help Ethiopia"
+            alt="Members of Help Ethiopia"
+            loading="lazy"
             className="w-full max-w-lg md:max-w-xl lg:max-w-2xl h-auto rounded-lg shadow-xl"
           />
         </div>
-
-        {/* Donation Info */}
         <div className="lg:w-1/2 rounded-lg xl:pt-8 xl:pl-8">
           <p className="text-md md:text-lg leading-8 md:leading-9 font-text">
             HELP Ethiopia is run by volunteers and contributions from our
@@ -33,12 +28,11 @@ export default function Home() {
           </p>
         </div>
       </div>
-
-      {/* Bank Details */}
       <div className="mt-16 mb-20 mx-auto max-w-lg bg-white shadow-lg rounded-xl p-6 text-center">
         <img
           src={abyssinia}
           alt="Abyssinia Bank Logo"
+          loading="lazy"
           className="w-[240px] max-w-full mx-auto mb-4"
         />
         <h2 className="text-2xl md:text-3xl font-text font-semibold text-gray-800 pb-6">
