9bf5c8f01ceb1dd46e73f1ce47f29b68a66c6087
Dagim <dagimgetaW27@gmail.com>
2025-02-20 18:06:41 +0300

    add blogs to the route

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import HomePage from "./Pages/HomePage";
 import NotFound from "./Components/NotFound/NotFound";
 import AboutPage from "./Pages/AboutPage";
 import TeamPage from "./Pages/TeamPage";
+import BlogsPage from "./Pages/BlogsPage";
 import WhatWeDo from "./Pages/WhatWeDo";
-import ContactPage from "./Pages/ContactPage";
 import DonatePage from "./Pages/DonatePage";
 
 export default function App() {
@@ -17,8 +17,8 @@ export default function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/our-team" element={<TeamPage />} />
+          <Route path="/blogs" element={<BlogsPage />} />
           <Route path="/what-we-do" element={<WhatWeDo />} />
-          <Route path="/contact" element={<ContactPage />} />
           <Route path="/donate" element={<DonatePage />} />
         </Route>
       </Routes>
