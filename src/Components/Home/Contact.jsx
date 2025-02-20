7d319a7932430b5cea208b3cfea254909e781bf3
Dagim <dagimgetaW27@gmail.com>
2025-02-20 16:26:53 +0300

    add contact section with form

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -1,53 +1,87 @@
-import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
-
 export default function Contact() {
   return (
-    <div className="py-12 px-30">
-      <h2 className="text-5xl font-semibold text-center font-text">
-        Contact Us
-      </h2>
-      <div className="flex justify-center items-center flex-wrap gap-10 pt-14 font-text">
-        <div className="flex flex-col justify-between w-full sm:w-3/4 md:w-1/2 xl:w-1/3">
-          <form className="shadow-lg shadow-form p-10 flex flex-col gap-1 rounded-md">
-            <label className="font-medium">Full Name</label>
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="border w-full mb-4 pl-2 border-[#1E3A8A] p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-[#1E3A8A]"
-            />
-            <label className="font-medium">Email</label>
-            <input
-              type="email"
-              placeholder="Email"
-              className="border w-full mb-4 pl-2 border-[#1E3A8A] p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-[#1E3A8A]"
-            />
-            <label className="font-medium">Message</label>
-            <textarea
-              placeholder="Message"
-              rows="4"
-              className="border w-full mb-4 pl-2 font-text border-[#1E3A8A] p-2 rounded-md focus:outline-none focus:ring-1 focus:ring-[#1E3A8A]"
-            ></textarea>
-            <div className="flex justify-center">
-              <button className="cursor-pointer mt-4 py-2 px-14 border border-[#1E3A8A] rounded-lg bg-[#1E3A8A] text-white">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="flex flex-col gap-10 font-text text-xl">
+    <div className="pb-18 px-15 md:px-20 xl:px-32 2xl:px-40 bg-gray-100 flex flex-col md:flex-row justify-center gap-12">
+      <div className="md:text-left flex-1 w-full md:w-1/2">
+        <h2 className="text-center md:text-left text-3xl font-text font-semibold text-gray-800 pb-10">
+          <span className="md:border-b-3 md:border-gray-800">Get In Touch</span>
+        </h2>
+        <div className="space-y-6 text-gray-800 font-text">
           <div className="flex items-center">
-            <FaMapMarkerAlt className="text-[#1E3A8A] text-3xl mr-4" />
-            <p>Addis Ababa, Ethiopia</p>
+            <img
+              src="https://img.icons8.com/?size=100&id=85353&format=png&color=000000"
+              alt="Location icon"
+              loading="lazy"
+              className="w-8 flex-shrink-0"
+            />
+            <p className="text-lg ml-3">
+              Tikur Ambessa Teaching Hospital, Zambia Street, Addis Ababa,
+              Ethiopia
+            </p>
           </div>
           <div className="flex items-center">
-            <FaPhoneAlt className="text-[#1E3A8A] text-3xl mr-4" />
-            <p>+251-912-345-678</p>
+            <img
+              src="https://img.icons8.com/?size=100&id=9730&format=png&color=000000"
+              alt="Phone icon"
+              loading="lazy"
+              className="w-8 flex-shrink-0"
+            />
+            <p className="text-lg ml-3">+251-912-345-678</p>
           </div>
           <div className="flex items-center">
-            <FaEnvelope className="text-[#1E3A8A] text-3xl mr-4" />
-            <p>contact@helpethiopia.org</p>
+            <img
+              src="https://img.icons8.com/?size=100&id=63489&format=png&color=000000"
+              alt="Envelope icon"
+              loading="lazy"
+              className="w-8 flex-shrink-0"
+            />
+            <p className="text-lg ml-3">contact@helpethiopia.org</p>
           </div>
         </div>
+      </div>
+      <div className="hidden md:block border-l-2 border-gray-300 h-64"></div>
+      <div className="flex-1 w-full md:w-1/2 space-y-4">
+        <div className="relative">
+          <img
+            src="https://img.icons8.com/?size=100&id=83190&format=png&color=000000"
+            alt="User icon"
+            loading="lazy"
+            className="w-6 flex-shrink-0 absolute top-3 left-3"
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
+          />
+        </div>
+        <div className="relative">
+          <img
+            src="https://img.icons8.com/?size=100&id=85500&format=png&color=000000"
+            alt="Email icon"
+            loading="lazy"
+            className="w-6 flex-shrink-0 absolute top-3 left-3"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
+          />
+        </div>
+        <div className="relative">
+          <img
+            src="https://img.icons8.com/?size=100&id=85962&format=png&color=000000"
+            alt="Pencil icon"
+            loading="lazy"
+            className="w-6 flex-shrink-0 absolute top-3 left-3"
+          />
+          <textarea
+            placeholder="Message"
+            rows="5"
+            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
+          ></textarea>
+        </div>
+        <button className="w-30 justify-center font-text flex m-auto bg-[#1E3A8A] text-white py-3 rounded-md">
+          Submit
+        </button>
       </div>
     </div>
   );
