b3d92dc55ed830365923d7cd0f0d18b47095e15b
Dagim <dagimgetaW27@gmail.com>
2025-02-28 23:11:27 +0300

    fix the contact form

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { MapPin, Phone, Mail, User, Pencil } from "lucide-react";
 
 export default function Contact() {
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [emailError, setEmailError] = useState("");
 
-  const isValidEmail = (email) => {
-    const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-    return regex.test(email);
-  };
+  // const isValidEmail = (email) => {
+  //   const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  //   return regex.test(email);
+  // };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setEmailError(isValidEmail(e.target.value) ? "" : "Invalid email address");
-  };
+  // const handleEmailChange = (e) => {
+  //   setEmail(e.target.value);
+  //   setEmailError(isValidEmail(e.target.value) ? "" : "Invalid email address");
+  // };
 
   const openGoogleMaps = () => {
     const url =
@@ -21,24 +22,19 @@ export default function Contact() {
   };
 
   return (
-    <div className="pb-18 px-10 md:px-20 xl:px-32 2xl:px-40 bg-gray-100 flex flex-col md:flex-row justify-center gap-12">
+    <div className="pb-18 px-10 md:px-20 xl:px-32 2xl:px-40 bg-gray-100 flex flex-col md:flex-row justify-center gap-12 font-text">
       <div className="md:text-left flex-1 w-full md:w-1/2">
-        <h2 className="text-3xl font-text font-semibold text-gray-800 pb-10">
+        <h2 className="text-3xl font-semibold text-gray-800 pb-10">
           <span className="border-b-3 border-gray-800">Get In Touch</span>
         </h2>
-        <div className="space-y-6 text-gray-800 font-text">
+        <div className="space-y-6 text-gray-800">
           <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/?size=100&id=85353&format=png&color=000000"
-              alt="Location icon"
-              loading="lazy"
-              className="w-8 flex-shrink-0"
-            />
+            <MapPin size={32} />
             <p className="text-lg ml-3">
               Tikur Ambessa Teaching Hospital, Zambia Street, Addis Ababa,
               Ethiopia{" "}
               <span
-                className="text-[#1E3A8A] font-semibold cursor-pointer font-text"
+                className="text-[#1E3A8A] font-semibold cursor-pointer border-b-2"
                 onClick={openGoogleMaps}
               >
                 Open In Maps
@@ -46,72 +42,52 @@ export default function Contact() {
             </p>
           </div>
           <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/?size=100&id=9730&format=png&color=000000"
-              alt="Phone icon"
-              loading="lazy"
-              className="w-8 flex-shrink-0"
-            />
+            <Phone size={32} />
             <p className="text-lg ml-3">+251-912-345-678</p>
           </div>
           <div className="flex items-center">
-            <img
-              src="https://img.icons8.com/?size=100&id=63489&format=png&color=000000"
-              alt="Envelope icon"
-              loading="lazy"
-              className="w-8 flex-shrink-0"
-            />
+            <Mail size={32} />
             <p className="text-lg ml-3">contact@helpethiopia.org</p>
           </div>
         </div>
       </div>
       <div className="hidden md:block border-l-2 border-gray-300 h-64"></div>
       <div className="flex-1 w-full md:w-1/2 space-y-4">
-        <div className="relative">
-          <img
-            src="https://img.icons8.com/?size=100&id=83190&format=png&color=000000"
-            alt="User icon"
-            loading="lazy"
-            className="w-6 flex-shrink-0 absolute top-3 left-3"
-          />
+        <div className="flex gap-3 shadow-md rounded-lg border border-gray-300 bg-white p-3">
+          <User className="text-gray-500 " />
           <input
             type="text"
+            name="firstName"
             placeholder="Name"
-            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
+            className="w-full rounded-md focus:outline-none"
           />
         </div>
-        <div className="relative">
-          <img
-            src="https://img.icons8.com/?size=100&id=85500&format=png&color=000000"
-            alt="Email icon"
-            loading="lazy"
-            className="w-6 flex-shrink-0 absolute top-3 left-3"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
-            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
-          />
-          {emailError && (
+        <div>
+          <div className="flex gap-3 shadow-md rounded-lg border border-gray-300 bg-white p-3">
+            <Mail className="text-gray-500 " />
+            <input
+              type="email"
+              name="email"
+              // value={email}
+              // onChange={handleEmailChange}
+              placeholder="Email"
+              className="w-full rounded-md focus:outline-none"
+            />
+          </div>
+          {/* {emailError && (
             <p className="text-red-500 text-sm mt-1">{emailError}</p>
-          )}
+          )} */}
         </div>
-        <div className="relative">
-          <img
-            src="https://img.icons8.com/?size=100&id=85962&format=png&color=000000"
-            alt="Pencil icon"
-            loading="lazy"
-            className="w-6 flex-shrink-0 absolute top-3 left-3"
-          />
+        <div className="flex gap-3 shadow-md rounded-lg border border-gray-300 bg-white p-3">
+          <Pencil className="text-gray-500 " />
           <textarea
-            placeholder="Message"
+            placeholder="Type your message..."
             rows="5"
-            className="w-full pl-12 p-3 rounded-md border border-gray-300 bg-white focus:outline-none"
+            aria-label="Message"
+            className="w-full rounded-md focus:outline-none"
           ></textarea>
         </div>
-        <button className="w-30 justify-center font-text flex m-auto bg-[#1E3A8A] text-white py-3 rounded-md">
+        <button className="w-30 justify-center flex m-auto bg-[#1E3A8A] text-white py-3 rounded-md cursor-pointer">
           Send
         </button>
       </div>
