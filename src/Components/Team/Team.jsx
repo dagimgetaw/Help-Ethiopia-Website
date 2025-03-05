45a0a3a2bcce8234bc2ef5c8d9c9f5a0fe5472b6
Dagim <dagimgetaW27@gmail.com>
2025-03-05 16:13:22 +0300

    add lucid react icons

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,9 +1,9 @@
-// import { FaFacebookF, FaLinkedinIn, FaGoogle } from "react-icons/fa";
+import { Facebook, Linkedin, Mails } from "lucide-react";
 import teamData from "./People";
 
 export default function Team() {
   return (
-    <div className="pt-40 pb-15 pl-10 pr-5 md:px-12 lg:px-20 xl:px-35">
+    <div className="pt-40 pb-15 pl-10 pr-5 md:px-12 lg:px-20 xl:px-35 bg-gray-50">
       <h2 className="text-3xl md:text-4x xl:text-5xl 2xl:text-6xl font-bold text-center text-gray-800">
         Meet Our Team!
       </h2>
@@ -29,27 +29,24 @@ export default function Team() {
             </h3>
             <p className="text-gray-600 text-sm font-text">{team.position}</p>
             <div className="flex justify-center gap-4 mt-4 text-[#1E3A8A] text-xl">
-              <img
-                src="https://img.icons8.com/?size=100&id=98972&format=png&color=000000"
-                alt="Facebook icon"
-                loading="lazy"
-                className="w-8 cursor-pointer"
+              <Facebook
+                className="cursor-pointer"
+                size={28}
+                color="#000000"
+                strokeWidth={1}
               />
-              <img
-                src="https://img.icons8.com/?size=100&id=98960&format=png&color=000000"
-                alt="Linkedin icon"
-                loading="lazy"
-                className="w-8 cursor-pointer"
+              <Linkedin
+                className="cursor-pointer"
+                size={28}
+                color="#000000"
+                strokeWidth={1}
               />
-              <img
-                src="https://img.icons8.com/?size=100&id=17935&format=png&color=000000"
-                alt="Google icon"
-                loading="lazy"
-                className="w-8 cursor-pointer"
+              <Mails
+                className="cursor-pointer"
+                size={28}
+                color="#000000"
+                strokeWidth={1}
               />
-              {/* <FaFacebookF className="hover:text-[#F1C40F] cursor-pointer" /> */}
-              {/* <FaLinkedinIn className="hover:text-[#F1C40F] cursor-pointer" /> */}
-              {/* <FaGoogle className="hover:text-[#F1C40F] cursor-pointer" /> */}
             </div>
           </div>
         ))}
