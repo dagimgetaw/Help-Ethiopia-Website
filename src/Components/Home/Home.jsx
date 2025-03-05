4a6ff145305a02b19366f556b5c53240ae166199
Dagim <dagimgetaW27@gmail.com>
2025-03-05 16:14:24 +0300

    comment home page donate btn

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="pt-80 pb-70 2xl:pt-90 text-center bg-gray-100">
+    <div className="pt-80 pb-70 2xl:pt-90 text-center bg-gray-50">
       <h2 className="pb-3 text-2xl text-gray-800 font-title">
         1000+ patients Helped
       </h2>
