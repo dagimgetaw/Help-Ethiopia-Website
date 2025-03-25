2eb71c5d141b73cba8b6afe660507a04e05b90a6
Dagim <dagimgetaW27@gmail.com>
2025-03-25 17:58:12 +0300

    blog pages

--- frontend/src/Pages/BlogsPage.jsx
+++ frontend/src/Pages/BlogsPage.jsx
@@ -1,11 +1,19 @@
-// import Home from "../Components/Contact/Home";
-import Info from "../Components/Blogs/Info";
+import Home from "../Components/Blogs/Home";
+import Recent from "../Components/Blogs/Recent";
+import Activities from "../Components/Blogs/Activities";
+import YoutubeChannel from "../Components/Blogs/Youtube";
+import Testimonial from "../Components/Blogs/Testimonia";
+// import Info from "../Components/Blogs/Info";
 
 export default function ContactPage() {
   return (
     <>
-      {/* <Home /> */}
-      <Info />
+      <Home />
+      <Recent />
+      <YoutubeChannel />
+      <Activities />
+      <Testimonial />
+      {/* <Info /> */}
     </>
   );
 }
