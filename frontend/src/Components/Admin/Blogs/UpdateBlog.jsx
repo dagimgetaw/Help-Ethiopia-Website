ae6f55ef9a1d0feabb9e47696234b08681ae84a3
Dagim <dagimgetaW27@gmail.com>
2025-03-26 20:49:05 +0300

    update blog

---
+++ frontend/src/Components/Admin/Blogs/UpdateBlog.jsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { Edit3, FileText, Timer } from "lucide-react";
+import axios from "axios";
+
+export default function UpdateBlog() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [time, setTime] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  const navigate = useNavigate();
+
+  // const formattedDate = new Date().toLocaleDateString("en-GB");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const res = await axios.put(`http://localhost:3000/updateblog/${id}`, {
+        title,
+        description,
+        time,
+      });
+
+      if (res.data.success) {
+        navigate("/admin/blogs");
+      }
+    } catch (error) {
+      console.log(
+        "Error uploading blog:",
+        error.response?.data || error.message
+      );
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 20000);
+    }
+  };
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/getblogbyid/${id}`
+        );
+        setBlog(response.data);
+        setTitle(response.data.title);
+        setDescription(response.data.description);
+        setTime(response.data.time);
+      } catch (error) {
+        console.error("Error fetching blog:", error);
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      }
+    };
+
+    fetchBlog();
+  }, [id]);
+
+  return (
+    <div className="pt-30 min-h-screen flex flex-col items-center bg-gray-50 font-text px-4 pb-20 relative">
+      {isLoading && (
+        <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+          <div className="Loading"></div>{" "}
+        </div>
+      )}
+
+      <h2 className="text-4xl md:text-5xl font-bold mt-6 text-gray-900 pb-6">
+        Update a Blog
+      </h2>
+      <div className="w-full max-w-3xl bg-white shadow-lg rounded-xl p-6 md:p-8 border border-gray-200">
+        <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+          <div className="relative flex items-center gap-3 shadow-sm rounded-lg border border-gray-300 bg-white p-4 focus-within:ring-2 focus-within:ring-blue-500 transition-all duration-300">
+            <Edit3 className="text-gray-500" size={24} />
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Blog's Title"
+              className="w-full outline-none text-gray-800 text-lg placeholder-gray-400 bg-transparent"
+            />
+          </div>
+          <div className="relative flex items-start gap-3 shadow-sm rounded-lg border border-gray-300 bg-white p-4 focus-within:ring-2 focus-within:ring-blue-500 transition-all duration-300">
+            <FileText className="text-gray-500" size={24} />
+            <textarea
+              name="description"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Blog description..."
+              rows="6"
+              className="w-full outline-none text-gray-800 text-lg placeholder-gray-400 bg-transparent resize-y"
+            />
+          </div>
+          <div className="relative flex items-center gap-3 shadow-sm rounded-lg border border-gray-300 bg-white p-4 focus-within:ring-2 focus-within:ring-blue-500 transition-all duration-300">
+            <Timer className="text-gray-500" size={24} />
+            <input
+              type="text"
+              name="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              placeholder="Time take's to read"
+              className="w-full outline-none text-gray-800 text-lg placeholder-gray-400 bg-transparent"
+            />
+          </div>
+
+          <button
+            className="w-full py-3 mt-6 rounded-lg bg-gradient-to-r bg-[#1E3A8A] text-white text-lg font-semibold cursor-pointer transition-all duration-300 hover:shadow-xl hover:scale-105"
+            type="submit"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
