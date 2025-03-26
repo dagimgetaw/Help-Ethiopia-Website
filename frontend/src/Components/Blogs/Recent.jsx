2507cfda9e188b94c17ae1758427d54cdd9813c5
Dagim <dagimgetaW27@gmail.com>
2025-03-26 21:06:42 +0300

    recent blog

--- frontend/src/Components/Blogs/Recent.jsx
+++ frontend/src/Components/Blogs/Recent.jsx
@@ -1,65 +1,69 @@
-const blogData = [
-  {
-    id: 1,
-    title: "Lorem ipsum dolor sit.",
-    date: "Jun 14 . 3 min read",
-    description:
-      "Lorem ipsum dolor sit amet cons adipisicing elit. Consectetur debitis impedit accusantium aliquid in voluptates.",
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/types-of-doctors-1600114658.jpg?crop=1xw:0.8425829875518672xh;center,top&resize=1200:*",
-    read: "Read more",
-  },
-  {
-    id: 2,
-    title: "Lorem ipsum dolor sit.",
-    date: "Jun 14 . 3 min read",
-    description:
-      "Lorem ipsum dolor sit amet cons adipisicing elit. Consectetur debitis impedit accusantium aliquid in voluptates.",
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/types-of-doctors-1600114658.jpg?crop=1xw:0.8425829875518672xh;center,top&resize=1200:*",
-    read: "Read more",
-  },
-  {
-    id: 3,
-    title: "Lorem ipsum dolor sit.",
-    date: "Jun 14 . 3 min read",
-    description:
-      "Lorem ipsum dolor sit amet cons adipisicing elit. Consectetur debitis impedit accusantium aliquid in voluptates.",
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/types-of-doctors-1600114658.jpg?crop=1xw:0.8425829875518672xh;center,top&resize=1200:*",
-    read: "Read more",
-  },
-];
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Recent() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/getblogs");
+        setBlogs(response.data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  // Function to truncate the description if it's more than 30 characters
+  const truncateDescription = (description) => {
+    return description.length > 120
+      ? description.substring(0, 120) + "..."
+      : description;
+  };
+
   return (
     <div className="pt-15 px-25 bg-gray-100 font-text">
       <p className="text-center text-gray-800 text-lg font-semibold mb-6 ">
         <span className="border-b-1 border-gray-800">Recent Posts</span>
       </p>
       <div className="pt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 mt-6">
-        {blogData.map((data) => (
+        {blogs.map((data) => (
           <div
-            key={data.id}
+            key={data.title}
             className="bg-white rounded-lg shadow-md overflow-hidden"
           >
             <img
-              src={data.image}
+              src={`http://localhost:3000/Images/${data.file}`}
               alt={data.title}
-              className="w-full h-50 object-cover"
+              className="w-full h-48 object-cover "
             />
-            <div className="p-4">
+            <div className="p-2">
               <h3 className="text-xl text-center font-semibold text-gray-900 pt-2 pb-2">
                 {data.title}
               </h3>
-              <p className="text-sm text-gray-600 pb-4">{data.date}</p>
-              <p className="text-gray-700 text-base mb-4">{data.description}</p>
-              <a
-                href="#"
-                className="text-[#1E3A8A] font-semibold underline cursor-pointer font-text text-base md:text-md"
-              >
-                {data.read}
-              </a>
+              <div className="px-2">
+                <p className="text-gray-700 text-base mb-4 pt-4">
+                  {truncateDescription(data.description)}{" "}
+                </p>
+                <div className="flex">
+                  <p className="text-sm text-gray-600 pb-4">
+                    {data.time} min read.
+                  </p>
+                  <p className="text-sm text-gray-600 pb-4 ml-auto">
+                    {data.date}
+                  </p>
+                </div>
+                <Link
+                  to={`/blogs/${data._id}`}
+                  className="text-[#1E3A8A] font-semibold underline cursor-pointer font-text text-base md:text-md"
+                >
+                  Read more
+                </Link>
+              </div>
             </div>
           </div>
         ))}
