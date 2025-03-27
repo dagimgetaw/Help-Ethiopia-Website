a976ce9c55bd1b7a057cafd07352410f4d8a1417
Dagim <dagimgetaW27@gmail.com>
2025-03-27 21:43:38 +0300

    add testimonial to homepage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import Home from "../Components/Home/Home";
 import Hero from "../Components/Home/Hero";
 import Service from "../Components/Home/Service";
-import News from "../Components/Home/News";
+// import News from "../Components/Home/News";
 import Stats from "../Components/Home/Stats";
 import Message from "../Components/Home/Message";
 import Partner from "../Components/Home/Partner";
 import FAQ from "../Components/Home/FAQ";
 import Contact from "../Components/Home/Contact";
+import Testimonial from "../Components/Home/Testimonial";
 
 export default function HomePage() {
   return (
@@ -14,11 +15,12 @@ export default function HomePage() {
       <Home />
       <Hero />
       <Service />
-      <News />
+      {/* <News /> */}
       <Stats />
       <Message />
       <Partner />
       <FAQ />
+      <Testimonial />
       <Contact />
     </>
   );
