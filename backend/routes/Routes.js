eb3abd0fdd22b0d5872825c3a8d32e58a332225b
Dagim <dagimgetaW27@gmail.com>
2025-04-05 23:03:36 +0300

    connect stripe routes

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -14,15 +14,25 @@ const {
   updateBlog,
   deleteBlog,
 } = require("../controller/blogController");
+const {
+  config,
+  createPaymentIntent,
+} = require("../controller/stripeController");
 
+// Authentication Routes
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/dashboard", verifyUser, dashboard);
 
+// Blog Routes
 router.post("/create", upload.single("file"), create);
 router.get("/getblogs", getBlogs);
 router.get("/getblogbyid/:id", getBlogById);
 router.put("/updateblog/:id", updateBlog);
 router.delete("/deleteblog/:id", deleteBlog);
+
+// Stripe Payment Routes
+router.get("/config", config);
+router.post("/create-payment-intent", createPaymentIntent);
 
 module.exports = router;
