16a59045336fe52d9a04d7847e02c042a782de35
Dagim <dagimgetaW27@gmail.com>
2025-04-05 23:21:41 +0300

    add Payment method to donate page

--- frontend/src/Pages/DonatePage.jsx
+++ frontend/src/Pages/DonatePage.jsx
@@ -1,5 +1,11 @@
 import Home from "../Components/Donate/Home";
+import PayMethods from "../Components/Donate/PayMethods";
 
 export default function DonatePage() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <PayMethods />
+    </>
+  );
 }
