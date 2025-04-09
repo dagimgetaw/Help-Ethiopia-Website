1f7ae0607eae6e0fe547703ead874abd2f2b0875
Dagim <dagimgetaW27@gmail.com>
2025-04-09 21:01:39 +0300

    feat: add save transaction func

--- backend/controller/chapaController.js
+++ backend/controller/chapaController.js
@@ -1,8 +1,33 @@
 require("dotenv").config();
-const chapa = require("chapa")(process.env.CHAPA_PUBLIC_KEY);
+const chapaModel = require("../models/chapaModel");
 
 const ChapaConfig = (req, res) => {
   res.send({ publishableKey: process.env.CHAPA_PUBLIC_KEY });
 };
 
-module.exports = { ChapaConfig };
+const saveChapaTransaction = async (req, res) => {
+  try {
+    const { firstName, lastName, email, phoneNumber, amount, tx_ref } =
+      req.body;
+
+    // Save to database
+    const newTransaction = new chapaModel({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      amount,
+      tx_ref,
+    });
+    await newTransaction.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Donation successful!",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { ChapaConfig, saveChapaTransaction };
