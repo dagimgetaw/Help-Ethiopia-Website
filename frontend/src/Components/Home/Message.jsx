35f1c325bcca07dd1fcc10c70527e383920967e7
Dagim <dagimgetaW27@gmail.com>
2025-04-12 20:27:15 +0300

    style: remove border and add padding on top for the btn

--- frontend/src/Components/Home/Message.jsx
+++ frontend/src/Components/Home/Message.jsx
@@ -6,9 +6,9 @@ export default function Message() {
       <h2 className="text-2xl md:text-3xl font-bold text-gray-700 mb-4 text-center">
         Register your company as our donor or sponsor and make a lasting impact.
       </h2>
-      <div className="flex justify-center">
+      <div className="flex justify-center pt-4">
         <Link to={"/register"}>
-          <button className="cursor-pointer py-2 px-12 m-auto border-1 font-text text-xl bg-blue-600 hover:bg-blue-700 rounded-lg text-white">
+          <button className="cursor-pointer py-2 px-12 m-auto font-text text-xl bg-blue-600 hover:bg-blue-700 rounded-lg text-white">
             Register
           </button>
         </Link>
