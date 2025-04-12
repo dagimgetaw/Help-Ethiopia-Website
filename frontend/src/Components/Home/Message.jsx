f9c83a910722ed115aac26d2c03ce2409185194b
Dagim <dagimgetaW27@gmail.com>
2025-04-12 19:58:10 +0300

    style: change btn color and title font size

--- frontend/src/Components/Home/Message.jsx
+++ frontend/src/Components/Home/Message.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Message() {
   return (
-    <div className="pt-10 pb-15 xl:pt-30 xl:pb-35 bg-gray-100">
-      <h2 className="text-2xl px-8 xl:text-3xl 2xl:text-4xl mt-4 text-center pb-14 font-text font-semibold text-gray-700">
+    <div className="py-16 px-6 md:px-12 lg:px-24 bg-gray-100 font-text">
+      <h2 className="text-2xl md:text-3xl font-bold text-gray-700 mb-4 text-center">
         Register your company as our donor or sponsor and make a lasting impact.
       </h2>
       <div className="flex justify-center">
         <Link to={"/register"}>
-          <button className="cursor-pointer py-2 px-12 m-auto border-1 font-text text-xl border-[#1E3A8A] rounded-lg bg-[#1E3A8A] text-white">
+          <button className="cursor-pointer py-2 px-12 m-auto border-1 font-text text-xl bg-blue-600 hover:bg-blue-700 rounded-lg text-white">
             Register
           </button>
         </Link>
