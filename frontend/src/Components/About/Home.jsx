6a153f999d22018e324354183f77a417796aab71
Dagim <dagimgetaW27@gmail.com>
2025-04-13 12:25:52 +0300

    style: change the width of the component

--- frontend/src/Components/About/Home.jsx
+++ frontend/src/Components/About/Home.jsx
@@ -1,21 +1,29 @@
 export default function Home() {
   return (
-    <div className="pt-30 pb-5 pl-10 pr-5 md:px-12 lg:px-20 xl:px-32 bg-gray-100 font-text">
-      <h2 className="text-3xl md:text-4xl xl:text-5xl font-semibold mt-4 text-center text-gray-800 pb-10">
-        About HELP ETHIOPIA
-      </h2>
-      <p className="text-left pt-10 text-md md:text-lg leading-8 md:leading-9 font-text">
-        HELP Ethiopia is a multidisciplinary non-profit organization that has
-        been registered and accorded legal personality in Ethiopia with registry
-        number 5623. Committed to improving healthcare, we collaborate with
-        local and international stakeholders to address neglected areas of
-        health service delivery and training. We also have a dedicated charity
-        program targeting economically disadvantaged groups of our society-
-        Ye’Eteye health Charity. Furthermore, we advocate for quality education,
-        leadership and collaboration as a tool for sustainable health care and
-        societal development. Hence the name “HELP” which stands for Health,
-        Education, Leadership and Partnership.
-      </p>
+    <div className="py-16 px-6 md:px-12 lg:px-24 xl:px-32 bg-gray-100 font-text">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 pt-15">
+            About HELP ETHIOPIA
+          </h2>
+        </div>
+
+        <div className="">
+          <p className="text-lg leading-8 md:leading-9 text-gray-700">
+            HELP Ethiopia is a multidisciplinary non-profit organization that
+            has been registered and accorded legal personality in Ethiopia with
+            registry number 5623. Committed to improving healthcare, we
+            collaborate with local and international stakeholders to address
+            neglected areas of health service delivery and training. We also
+            have a dedicated charity program targeting economically
+            disadvantaged groups of our society- Ye&apos;Eteye health Charity.
+            Furthermore, we advocate for quality education, leadership and
+            collaboration as a tool for sustainable health care and societal
+            development. Hence the name &quot;HELP&quot; which stands for
+            Health, Education, Leadership and Partnership.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
