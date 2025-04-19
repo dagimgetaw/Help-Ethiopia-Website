683a9992da070741fa192a2f68eb30850c1315cf
Dagim <dagimgetaW27@gmail.com>
2025-04-19 23:56:50 +0300

    feat: remove acitivity from blog

--- frontend/src/Pages/BlogsPage.jsx
+++ frontend/src/Pages/BlogsPage.jsx
@@ -1,9 +1,6 @@
 import Home from "../Components/Blogs/Home";
 import Recent from "../Components/Blogs/Recent";
-import Activities from "../Components/Blogs/Activities";
 import YoutubeChannel from "../Components/Blogs/Youtube";
-import Testimonial from "../Components/Blogs/Testimonia";
-// import Info from "../Components/Blogs/Info";
 
 export default function ContactPage() {
   return (
@@ -11,9 +8,6 @@ export default function ContactPage() {
       <Home />
       <Recent />
       <YoutubeChannel />
-      <Activities />
-      <Testimonial />
-      {/* <Info /> */}
     </>
   );
 }
