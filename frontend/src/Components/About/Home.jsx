80c86f983de9382be266d6a4df14919e3b37a373
Dagim <dagimgetaW27@gmail.com>
2025-04-20 22:08:13 +0300

    feat: update the content

--- frontend/src/Components/About/Home.jsx
+++ frontend/src/Components/About/Home.jsx
@@ -8,21 +8,19 @@ export default function Home() {
           </h2>
         </div>
 
-        <div className="">
-          <p className="text-lg leading-8 md:leading-9 text-gray-700">
-            HELP Ethiopia is a multidisciplinary non-profit organization that
-            has been registered and accorded legal personality in Ethiopia with
-            registry number 5623. Committed to improving healthcare, we
-            collaborate with local and international stakeholders to address
-            neglected areas of health service delivery and training. We also
-            have a dedicated charity program targeting economically
-            disadvantaged groups of our society- Ye&apos;Eteye health Charity.
-            Furthermore, we advocate for quality education, leadership and
-            collaboration as a tool for sustainable health care and societal
-            development. Hence the name &quot;HELP&quot; which stands for
-            Health, Education, Leadership and Partnership.
-          </p>
-        </div>
+        <p className="text-lg leading-8 md:leading-9 text-gray-700">
+          HELP Ethiopia is a multidisciplinary non-profit organization that has
+          been registered and accorded legal personality in Ethiopia with
+          registry number 5623. Committed to improving healthcare, we
+          collaborate with local and international stakeholders to address
+          neglected areas of health service delivery and training. We also have
+          a dedicated charity program targeting economically disadvantaged
+          groups of our society- Ye&apos;Eteye health Charity. Furthermore, we
+          advocate for quality education, leadership and collaboration as a tool
+          for sustainable health care and societal development. Hence the name
+          &quot;HELP&quot; which stands for Health, Education, Leadership and
+          Partnership.
+        </p>
       </div>
     </div>
   );
