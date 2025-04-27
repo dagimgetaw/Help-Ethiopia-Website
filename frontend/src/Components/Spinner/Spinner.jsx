9dbbeeba4b29d16b4ac6b754872eac82323ec993
Dagim <dagimgetaW27@gmail.com>
2025-04-27 21:41:18 +0300

    style: change the spinner color

--- frontend/src/Components/Spinner/Spinner.jsx
+++ frontend/src/Components/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import { HashLoader } from "react-spinners";
 export default function Spinner() {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-4">
-      <HashLoader color="#1E3A8A" size={60} />
+      <HashLoader color="#2563eb" size={60} />
       <p className="text-lg pt-2 font-semibold text-gray-700 font-text">
         Loading, please wait...
       </p>
